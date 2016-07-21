fb40c56430b7ab20823e9b0d6fbd023f612421d6
freef49 <sfiedler@gmail.com>
2016-07-21 16:10:04 +1000

    working example email form

--- examples.php
+++ examples.php
@@ -1,17 +1,24 @@
 <?php // upload.php
   require_once 'login.php';
 
-
-  if (isset($_POST['name'])) $name = $_POST['name'];
-  elseif (isset($_POST['name'])== '') {
-    $name = "(Not entered)";
-    require_once 'login.php';
   $conn = new mysqli($hn, $un, $pw, $db);
   if ($conn->connect_error) die($conn->connect_error);
-$query = "UPDATE cats SET name='Charlie' WHERE name='Charly'"; $result = $conn->query($query);
-if (!$result) die ("Database access failed: " . $conn->error);
+
+  if (isset($_POST['first_name'])&&
+      isset($_POST['last_name'])&&
+      isset($_POST['email'])
+    ) 
+  {
+    $first_name = $_POST['first_name'];
+    $last_name = $_POST['last_name'];
+    $email = $_POST['email'];
+
+    $query = "INSERT INTO emails VALUES ('$first_name','$last_name','$email')";
+
+    $result = $conn->query($query);
+    if (!$result) die ("Database access failed: " . $conn->error);
   }
-  else $name = "(Not entered)";
+
 echo <<<_END
   <html>
     <head>
@@ -19,14 +26,30 @@ echo <<<_END
       <link rel="stylesheet" type="text/css" href="bootstrap/bootstrap.css"/>
     </head>
     <body>
-      Your name is: $name<br>
+
+      <p>Your name is: $first_name $last_name and your email is $email</p>
+
       <form method="post" action="examples.php">
         <p>What is your <b>first</b> name?<input type="text" name="first_name"></p>
         <p>What is your <b>last</b> name?<input type="text" name="last_name"></p>
         <p>What is your <b>email</b>?<input type="text" name="email"></p>
-        <input type="submit">
+        <input type="submit" id="submitButton">
       </form>
+
       <script src="bootstrap/bootstrap.js">
+
+      <script
+        src="https://code.jquery.com/jquery-3.1.0.min.js"
+        integrity="sha256-cCueBR6CsyA4/9szpPfrX3s49M9vUU5BgtiJj06wt/s="
+        crossorigin="anonymous">
+      </script>
+
+
+      <script>
+        $("#submitButton").click( function( event ){
+            event.preventDefault();
+        });
+      </script>
     </body>
   </html>
 _END;
--- login.php
+++ login.php
@@ -2,5 +2,5 @@
 	$hn = 'localhost'; 
 	$un = 'root'; 
 	$pw = 'Biology1';	
-	$db = 'publications'; 
+	$db = 'contacts'; 
 ?>
