44adef0d13e3ae503ae2cb279a5ddf0baa8adeb6
freef49 <sfiedler@gmail.com>
2016-07-24 10:40:37 +1000

    email sign up with sanitization

--- examples.php
+++ examples.php
@@ -1,6 +1,12 @@
 <?php // upload.php
   require_once 'login.php';
-
+  
+  function mysql_fix_string($conn, $string)
+  {
+    if (get_magic_quotes_gpc()) $string = stripslashes($string);
+    return $conn->real_escape_string($string);
+  }
+  
   $conn = new mysqli($hn, $un, $pw, $db);
   if ($conn->connect_error) die($conn->connect_error);
 
@@ -9,11 +15,12 @@
       isset($_POST['email'])
     ) 
   {
-    $first_name = $_POST['first_name'];
-    $last_name = $_POST['last_name'];
-    $email = $_POST['email'];
-
-    $query = "INSERT INTO emails VALUES ('$first_name','$last_name','$email')";
+    
+    $first_name_sanitized  = mysql_fix_string($conn, $_POST['first_name']);
+    $last_name_sanitized  = mysql_fix_string($conn, $_POST['last_name']);
+    $email_sanitized = mysql_fix_string($conn, $_POST['email']);
+    
+    $query = "INSERT INTO emails VALUES ('$first_name_sanitized','$last_name_sanitized','$email_sanitized')";
 
     $result = $conn->query($query);
     if (!$result) die ("Database access failed: " . $conn->error);
@@ -29,7 +36,7 @@ echo <<<_END
 
       <p>Your name is: $first_name $last_name and your email is $email</p>
 
-      <form method="post" action="examples.php">
+      <form method="post" action="index.php">
         <p>What is your <b>first</b> name?<input type="text" name="first_name"></p>
         <p>What is your <b>last</b> name?<input type="text" name="last_name"></p>
         <p>What is your <b>email</b>?<input type="text" name="email"></p>
@@ -53,6 +60,5 @@ echo <<<_END
     </body>
   </html>
 _END;
-
 
 ?>
